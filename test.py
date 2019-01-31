f9410fad1d16ba1e90d3e08e8c57b87290d9a76c
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-01-31 17:46:55 +0200

    Add multithreading get request support from service

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ service = request.APIService("clientInfo.txt")
 
 geo1 = model.GeoPoint(30.457461388888888,50.448696388888884)
 geo2 = model.GeoPoint(30.456461388888888,50.458696388888884)
-img = service.createImageRequest().search().addBbox(geo1, geo2).addPano(False).addRadius(1000).addPerPage(1000).get()
+imgs = service.createImageRequest().search().addBbox(geo1, geo2).addPano(False).addRadius(225).addPerPage(225).get()
 
-service.createImageRequest().download(img, 640, "imgs/", 15).get()
+requests = service.createDownloadImagesRequests(imgs, 320, "imgs/")
+service.multithreadingGetRequests(requests, 15)
