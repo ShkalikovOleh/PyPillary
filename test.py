7ce165a324ed0c1524a14f8bfcaa4d19efe9f3fe
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-01-31 16:16:39 +0200

    Add DownoloadRequest, ovveride get and parse search response

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ service = request.APIService("clientInfo.txt")
 geo1 = model.GeoPoint(30.457461388888888,50.448696388888884)
 geo2 = model.GeoPoint(30.456461388888888,50.458696388888884)
 img = service.createImageRequest().search().addBbox(geo1, geo2).get()
-print(img.json())
+print(len(img))
 
-#request.downoloadImage(img, 1024, "")
+service.createImageRequest().download(img[0], 640, "").get()
