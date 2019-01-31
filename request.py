b0a69d21f98d90e44eaab2f0ede476f26ed2420c
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-01-31 10:45:23 +0200

    Create base fluent request api for image request

--- request.py
+++ request.py
@@ -1,29 +1,83 @@
 import model
 import requests
+import time
 
-apiPrefix = "https://a.mapillary.com/v3/"
-cdnPrefix = "https://d1cuyjsrcm0gby.cloudfront.net/"
-clientId = ""
-clientSecret = ""
-imagePrefix = "images/"
-sequencePrefix = "sequences/"
+sequencePrefix = "sequences"
 andPart = "&"
 answerPart = "?"
-imageResolutions = {320: "/thumb-320.jpg",
+comaPart = ","
+
+
+class APIRequest:
+    _apiPrefix = "https://a.mapillary.com/v3/"
+    
+    def __init__(self, clientId, clientSecret):
+        self._clientId = clientId
+        self._clientSecret = clientSecret
+        self._requestString = ""
+
+    @property
+    def requestString(self):
+        return self._requestString
+
+
+    @property
+    def response(self):
+        return self._response
+
+
+    def checkAnd(self):
+        if self._requestString[-1] != '/' and self._requestString[-1] != '&':        
+            self._requestString += andPart
+
+
+    def get(self):
+        self.checkAnd()
+        self._requestString += ("client_id=" + self._clientId)
+        self._response = requests.get(self._requestString)
+        return self._response
+
+
+class ImageRequest(APIRequest):    
+    _imagePrefix = "images"
+    _cdnPrefix = "https://d1cuyjsrcm0gby.cloudfront.net/"
+    _imageResolutions = {320: "/thumb-320.jpg",
                     640: "/thumb-640.jpg",
                     1024: "/thumb-1024.jpg",
-                    2048: "/thumb-320.jpg}"}
+                    2048: "/thumb-2048.jpg}"}
 
 
-def loadClientInfo(path):
-    global clientId
-    global clientSecret
-    with open(path, "r") as file:
-        clientId = file.readline()
-        clientSecret = file.readline()
+    def search(self):
+        self._requestString += (self._apiPrefix + self._imagePrefix + answerPart)
+        isr = ImageSearchRequest(self._clientId, self._clientSecret)
+        isr._requestString = self._requestString
+        return isr
 
 
-def downoloadImage(image, resolution, directoryPath):    
-    response = requests.get(cdnPrefix + image.key + imageResolutions[resolution])
-    with open(directoryPath + image.getFilename, "wb") as file:
-        file.write(response.content)
+class ImageSearchRequest(APIRequest):
+    def addBbox(self, geoPointMin, geoPointMax):
+        self.checkAnd()
+        self._requestString += ("bbox=" + str(geoPointMin.longitude) + comaPart + 
+            str(geoPointMin.latitude) + comaPart + str(geoPointMax.longitude) + 
+            comaPart + str(geoPointMax.latitude))
+        return self
+
+
+    def addStartTime(self, datetime):
+        '''
+        param datetime is datatime type object
+        '''
+        self.checkAnd()
+        self._requestString += ("start_time=" + datetime.isoformat())
+        return self
+
+
+class APIService:
+    def __init__(self, credPath):
+        with open(credPath, "r") as file:
+            self._clientId = file.readline().replace("\n", "") 
+            self._clientSecret = file.readline().replace("\n", "")
+
+
+    def createImageRequest(self):
+        return ImageRequest(self._clientId, self._clientSecret)
