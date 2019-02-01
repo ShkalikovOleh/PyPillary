ce2a22bba6ab30f7b9d6ad3be10b6d6435701f2c
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-02-01 12:03:50 +0200

    Add createImageRequests

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ service = request.APIService("clientInfo.txt")
 
 geo1 = model.GeoPoint(30.457461388888888,50.448696388888884)
 geo2 = model.GeoPoint(30.456461388888888,50.458696388888884)
-imgsReq = service.createImageSearchRequest().addBbox(geo1, geo2). \
-        addPano(False).addRadius(225).addPerPage(200)
+imgsReq = service.createImageSearchRequest().addCloseTo(geo2). \
+        addPano(False).addRadius(200).addPerPage(200)
 imgs = imgsReq.get()
-img = service.createImageRequest().key(imgs[0].key).get()
+img = service.createImageRequest(imgs[0].key).get()
 
-requests = service.createDownloadImagesRequests(imgs, 320, "imgs/")
-service.multithreadingGetRequests(requests, 30)
+requests = service.createDownloadImagesRequests(imgs[100:200], 320, "imgs/")
+service.multithreadingGetRequests(requests, 10)
