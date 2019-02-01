91c91217eca181b100930cd2d1c93b1f2add3d5e
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-02-01 10:39:17 +0200

    Separate image requests in different classes

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import model
 import request
-import time
 
 service = request.APIService("clientInfo.txt")
 
 geo1 = model.GeoPoint(30.457461388888888,50.448696388888884)
 geo2 = model.GeoPoint(30.456461388888888,50.458696388888884)
-imgs = service.createImageRequest().search().addBbox(geo1, geo2).addPano(False).addRadius(225).addPerPage(225).get()
+imgs = service.createImageSearchRequest().addBbox(geo1, geo2). \
+        addPano(False).addRadius(225).addPerPage(15).get()
+img = service.createImageRequest().key(imgs[0].key).get()
+print(img.username)
 
 requests = service.createDownloadImagesRequests(imgs, 320, "imgs/")
 service.multithreadingGetRequests(requests, 15)
